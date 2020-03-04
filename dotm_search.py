0d7b3612ac83756fd4ae2dc58ca5c826b261fbca
Sean Bailey <sepabailey@gmail.com>
2020-03-04 11:36:27 -0500

    output returns correct info

--- dotm_search.py
+++ dotm_search.py
@@ -5,11 +5,48 @@
 Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-__author__ = "???"
+__author__ = "Sean Bailey, Janell Huyck, Koren Niles, Chris Wilson, Julita Marshall"
+
+import os
+import zipfile
+
+
+def grabfile(directories):
+    hold_files = []
+    for root, directories, files in os.walk(directories):
+        for filename in files:
+            fullPath = os.path.join(root, filename)
+            hold_files.append(fullPath)
+    return hold_files
+
+
+def readfile(file_name, text):
+    file = zipfile.ZipFile(file_name)
+    track_matches = 0
+    with file.open('word/document.xml') as f:
+        for line in f:
+            txt = line.find(text)
+            while txt != -1:
+                print(line[txt - 40: txt + 40])
+                txt = line.find('$', txt + 1)
+                track_matches += 1
+    return track_matches
 
 
 def main():
-    raise NotImplementedError("Your awesome code begins here!")
+    run_path = grabfile('./dotm_files')
+    file_total = 0
+    match_total = 0
+
+    for x in run_path:
+        if(x.endswith('.dotm')):
+            file_total += 1
+            matches = readfile(x, '$')
+            if (matches > 0):
+                match_total += 1
+            else:
+                continue
+    print(match_total)
 
 
 if __name__ == '__main__':
